c816019adf748dd0695b91a4e8844e354a99f50d
ndmx <alexanderukaga@gmail.com>
2022-07-28 04:32:38 -0400

    changed a line

--- get_sum_average.py
+++ get_sum_average.py
@@ -1,8 +1,13 @@
-values = [23, 52, 59, 37, 48 ]
+input_string = input("Enter list elements seperated by space ")
+print("\n")
+user_list = input_string.split()
+
+print("list: ", user_list)
+
 sum = 0
 length = 0
-for value in values:
-    sum +=value
+for value in user_list:
+    sum +=int(value)
     length +=1
 
 print("Total sum: " + str(sum) + " - Average: " + str(sum/length))
