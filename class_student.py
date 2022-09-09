09410aeace8a14665f2b2128e57449910c86f92e
ndmx <alexanderukaga@gmail.com>
2022-09-09 01:31:10 -0300

    tried following with pycharm

--- class_student.py
+++ class_student.py
@@ -1,19 +1,31 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
         self.name = name
         self.house = house
+    def __str__(self):
+        return f"{self.name} from {self.house}"
 
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student(name, house)
-    return student
+    return Student(name, house)
 
 
 if __name__ == '__main__':
